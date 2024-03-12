6f35a96131aaf98ca7cf01dce5dbe3a132481605
macosta2015 <macosta2015@my.fit.edu>
2024-03-12 00:14:43 -0400

    Commit test

--- test.js
+++ test.js
@@ -2,28 +2,27 @@ const puppeteer = require('puppeteer');
 
 (async () => {
     try {
-        const browser = await puppeteer.launch({ headless: false }); // Launch Chrome with visible browser window
-        const page = await browser.newPage();
+        const browser = await puppeteer.launch({
+            headless: false, // Launch Chrome with visible browser window
+            userDataDir: '/Users/marioacosta/Library/Application Support/Google/Chrome/Profile 1' // Path to your user data directory
+        });
 
-        // Navigate to Google
-        const navigationPromise = page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 });
-        await page.goto('https://cad.onshape.com/documents?resourceType=resourcecompanyowner&nodeId=65efc5e06e5bec02f57742fe', { waitUntil: 'networkidle0', timeout: 0 });
-        await navigationPromise;
+        const pages = await browser.pages(); // Get all open pages (tabs)
 
-        // You can add further interactions here, such as searching for something on Google
-        // For example, searching for "Puppeteer tutorial"
-        await page.type('input[name="q"]', 'Puppeteer tutorial'); // Type the search query
-        await page.keyboard.press('Enter'); // Press the Enter key
+        // Open a new tab
+        const newPage = await browser.newPage();
 
-        // Wait for search results to load
-        await page.waitForSelector('#search');
+        // Navigate the new tab to a URL
+        await newPage.goto('https://cad.onshape.com/documents?resourceType=resourcecompanyowner&nodeId=65efc5e06e5bec02f57742fe', { waitUntil: 'networkidle0', timeout: 0 });
 
-        // Capture a screenshot of the search results page
-        await page.screenshot({ path: 'google_search_results.png' });
+        // You can add further interactions here
 
-        // Close the browser
+        // Close the browser when done
         await browser.close();
     } catch (error) {
         console.error('An error occurred:', error);
     }
 })();
+
+
+
