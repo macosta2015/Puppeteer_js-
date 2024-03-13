18fd34a1cf352e1e4599feaba2d2b140f0b50fd8
macosta2015 <macosta2015@my.fit.edu>
2024-03-13 16:25:53 -0400

    commit

--- test.js
+++ test.js
@@ -15,10 +15,22 @@ const puppeteer = require('puppeteer');
         // Navigate the new tab to a URL
         await newPage.goto('https://cad.onshape.com/documents?resourceType=resourcecompanyowner&nodeId=65efc5e06e5bec02f57742fe', { waitUntil: 'networkidle0', timeout: 0 });
 
-        // You can add further interactions here
+        // Fill out the form fields
+        await newPage.type('input[name="email"].form-control', 'macosta1297@gmail.com');
+        await newPage.type('input[name="password"].form-control', 'ElonMusk2050');
 
-        // Wait for 10 seconds before closing the browser
+        // Click on the submit button
+        await newPage.click('button.btn.btn-primary.os-signin-button');
+        await new Promise(resolve => setTimeout(resolve, 5000));
+
+        // Optionally, you can wait for navigation to complete
+        await newPage.waitForNavigation();
+
+        // Add a timer to wait for 10 seconds after clicking the submit button
         await new Promise(resolve => setTimeout(resolve, 10000));
+
+        // Take a screenshot to verify the form submission
+        await newPage.screenshot({ path: 'form_submission.png' });
 
         // Close the browser
         await browser.close();
@@ -26,5 +38,3 @@ const puppeteer = require('puppeteer');
         console.error('An error occurred:', error);
     }
 })();
-
-
